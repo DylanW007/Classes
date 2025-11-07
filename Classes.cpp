1ff27c24bfc7c2b2febca31ce9eabf09d998de1a
U-BSD\453480 <453480@SHS-GYBF6X3.beaverton.k12.or.us>
2025-11-07 09:59:05 -0800

    Save2Example

--- Classes.cpp
+++ Classes.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+#include <cstring>
+#include "Room.h"
+#include <vector>
+
 using namespace std;
 
+const in SIZE = 2;
+
+void printArrayDescriptions(Room**, int);
+void printVectorDescriptions(Vector<Room*> &);
+
 int main(){
-  cout << "Hello";
+  Room* list[SIZE];
+  vector<Room*> vect;
+  char* firstDescription = new char[80];
+  strcpy(firstDescription, "First Room");
+  list[0] = new Room(firstDescription);
+  char* secondDescription = new char[80];
+  strcpy(secondDescription, "Second Room");
+  list[1] = new Room(secondDescriptions);
+  printArrayDescriptions(list, SIZE);
+  vect.push_back(list[0]);
+  vect.push_back(list[1]);
+  printVectorDescriptons(vect);
+}
+
+void printArrayDescriptions(Room** newlist, in size) {
+  for (int a = 0; a < size; a++){
+    cout << newlist[a]->getDescription() << endl;
+    //cout << *newlist[a]->getDescriptions() << endl;
+  }
+}
+
+void printVectorDescriptions(vector<Room*> &newvect){
+  for(vector<Room*>::iterator it = newvect.begin(); it != newvect.end(); it++){
+    cout << (*it)->getDescription() << endl;
+    //cout << *(*it)->getDescripton() << endl;
+  }
 }
