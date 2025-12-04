7fc1ee464f4e98703a7c65c31cc987f464921f9a
U-BSD\453480 <453480@SHS-GYBF6X3.beaverton.k12.or.us>
2025-12-04 09:57:04 -0800

    Little changes

--- main.cpp
+++ main.cpp
@@ -1,50 +1,66 @@
-//   
 // Dylan Waters
 //
 // Classes (11/19/25)
 //
 // Description...
 
+ #include <iostream>
+ #include <cstring>
  #include <vector>
  #include "media.h"
  using namespace std;
+
+ const int SIZE = 6;
 
  int main()
  {
     // Create a vector to hold media pointers
     vector<Media*> mediaCollection;
+    
+    char* firstDescription = new char[80];
+    strcpy(firstDescription, "First Media");
+    list[0] = new Media(firstDescription);
+    char* secondDescription = new char[80];
+    strcpy(secondDescription, "Second Media");
+    list[1] = new Media(secondDescription);
 
-    //ADD
-    //SEARCH
-    //DELETE
+    printMedia(list, SIZE);
+    mediaCollection.push_back(list[0]);
+    mediaCollection.push_back(list[1]);
+    //printVectorDescription(mediaCollection);
 
+    bool run;
+    run = true;
+
+    while(run == true){
+      cout << "What is your command?: ";
+    }
+    //COMMANDS:
+    //         ADD
+    //         SEARCH
+    //         DELETE
+
+    
+    
+    return 0;
  }
 
-    // Example:
-    // Music* song = new Music();
-    // song->SetTitle("Song Title");
-    // song->SetYear(2021);
-    // mediaCollection.push_back(song);
-
-void searchMedia(){
+//void searchMedia(){
   // Search Media
-  for (Media* media : mediaCollection) {
+  //for (Media* media : mediaCollection) {
   
-  }
-}
+    //}
+  //}
 
-void printMedia(){    // Iterate through the collection and print details
-    for (Media* media : mediaCollection) {
-        media->Print();
+void printMedia(Media** newlist, int size){
+  for (int a = 0; a < size; a++) {
+    cout << newlist[a]->getTitle() << endl;
     }
 }
 
-void deleteMedia(){
+//void deleteMedia(){
     // Clean up memory
-    for (Media* media : mediaCollection) {
-        delete media;
-    }
-}
-
-    return 0;
-}
+    //for () {
+  //delete media;
+  //}
+  //}
