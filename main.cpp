3f5ed9c7ce343b2d5e30b8d07235aba3e3e2deb1
DylanW007 <453480@bsd48.org>
2025-12-10 00:17:38 -0800

    Cleaned up

--- main.cpp
+++ main.cpp
@@ -4,54 +4,78 @@
 //
 // Description...
 
- #include <iostream>
- #include <cstring>
- #include <vector>
- #include "media.h"
- using namespace std;
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <bits/stdc++.h> // for converting command to lowercase.
+#include "media.h"
+using namespace std;
 
- const int SIZE = 6;
+const int SIZE = 6;
 
- void printMedia(Media** newlist, int size){
-   for(int a = 0; a < size; a++){
-     cout << newlist[a]->getTitle() << endl;
-   }
- }
-
- int main()
- {
-    // Create a vector to hold media pointers
+int main()
+{
     vector<Media*> mediaCollection;
     
     char* firstDescription = new char[80];
-
+    
     bool run;
     run = true;
 
+    string command;
     while(run == true){
-      cout << "What is your command?: ";
-    }
-    //COMMANDS:
-    //         ADD
-    //         SEARCH
-    //         DELETE
+      
+       cout << "What is your command? (Type help for commands): ";
+       cin >> command;
 
+       for(auto& x : command){ // https://www.geeksforgeeks.org/cpp/tolower-function-in-cpp/
+          x = tolower(x);    // (Converts string tolower() using for loop)
+       }
+       
+      
+       if (command == "add"){
+           cout << "Add" << endl;
+           //addMedia();
+       }
+      
+       else if (command == "search"){
+           cout << "Search" << endl;
+           //searchMedia();
+       }
     
+       else if (command == "delete"){
+           cout << "Delete" << endl;
+           //deleteMedia();
+       }
+       
+       else if (command == "help"){
+           cout << "\nCommands:" << endl;
+           cout << "========" << endl;
+           cout << "Add:" << endl;
+           cout << "Search:" << endl;
+           cout << "Delete:" << endl;
+           cout << "========\n" << endl;
+       }
+      
+       else{
+           cout << "Not a valid input." << endl;
+       }
+        
+    }
     
     return 0;
+}
+
+void addMedia(Media** newlist, int size){
+    // Add Media
  }
 
-//void searchMedia(){
+
+void searchMedia(){
   // Search Media
-  //for (Media* media : mediaCollection) {
-  
-    //}
-//}
+}
 
 
-//void deleteMedia(){
-    // Clean up memory
-    //for () {
-  //delete media;
-  //}
-  //}
+void deleteMedia(){
+    // Delete Media
+}
