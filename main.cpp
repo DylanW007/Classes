a3da602c6cb4abc72c273842e13beeeda13f3cd7
U-BSD\453480 <453480@SHS-GYBF6X3.beaverton.k12.or.us>
2025-12-10 10:26:29 -0800

    save

--- main.cpp
+++ main.cpp
@@ -15,16 +15,71 @@ const int SIZE = 6;
 
 void addMedia(vector<Media*>& collection){ // Moved above main
     // Add Media
-    cout << "What type of media (Help for media types): " << endl;
+
+    char mediatype[50];
+    char title[50];
+    int year;
+  
+    cout << "What type of media (Help for media types): ";
     cin >> mediatype;
-    
-    cout << "Enter the Title: ";
-    cin.getline(title, 80); // https://www.geeksforgeeks.org/cpp/getline-string-c/
-    
-    cout << "Enter the Year: ";
-    cin >> year;
-    
-    //collection.push_back
+
+    for(auto& m : mediatype){
+      m = tolower(m);
+    }
+
+    if (strcmp(mediatype, "music") == 0){
+      bool check = true;
+      while (check = true){
+	char askcheck;
+	char title[50];
+	int year;
+	char publisher[50];
+	float rating;
+	
+	cout << "What is the title?: ";
+	cin >> title;
+	cout << "What is the year?: ";
+	cin >> year;
+        cout << "Who is the publisher?: ";
+	cin >> publisher;
+	cout << "What is the rating?: ";
+	cin >> rating;
+
+        cout << "\nMusic:" << endl;
+	cout << "======" << endl;
+	cout << title << endl;
+	cout << year << endl;
+	cout << publisher << endl;
+	cout << rating << endl;
+	
+	cout << "Does this look right (y/n)?: " << endl;
+        cin >> askcheck;
+
+	if (askcheck == 'y'){
+	  check = false;
+	}
+	else if (askcheck == 'n'){
+	   char askcheck = '\0';
+	   cout << "Redo'ing" << endl;
+	}
+	else{
+	  cout << "Not valid" << endl;
+	}
+	
+      }
+    }
+
+    else if (mediatype == "videogame"){
+
+    }
+
+    else if (mediatype == "movie"){
+
+    }
+
+    else{
+      cout << "Not a valid media input" << endl;
+    }
 }
 
 //void searchMedia(){
@@ -45,7 +100,7 @@ int main()
     bool run;
     run = true;
 
-    string command;
+    char command[50];
     while(run == true){
       
        cout << "What is your command? (Type help for commands): ";
@@ -54,24 +109,24 @@ int main()
        for(auto& x : command){ // https://www.geeksforgeeks.org/cpp/tolower-function-in-cpp/
           x = tolower(x);    // (Converts string tolower() using for loop)
        }
+
        
-      
-       if (command == "add"){
+       if (strcmp(command, "add") == 0){
            //cout << "Add" << endl;
            addMedia(mediaCollection);
        }
       
-       else if (command == "search"){
+       else if (strcmp(command, "search") == 0){
            cout << "Search" << endl;
            //searchMedia();
        }
     
-       else if (command == "delete"){
+       else if (strcmp(command, "delete") == 0){
            cout << "Delete" << endl;
            //deleteMedia();
        }
        
-       else if (command == "help"){
+       else if (strcmp(command, "help") == 0){
            cout << "\nCommands:" << endl;
            cout << "========" << endl;
            cout << "Add:" << endl;
