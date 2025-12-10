cb65a1c6f23f53e7acdae82dec8ea524c2b9de93
DylanW007 <453480@bsd48.org>
2025-12-10 09:30:58 -0800

    slightly exapanded

--- media.h
+++ media.h
@@ -10,7 +10,7 @@
  *      - title, year, publisher, rating
  */
  
-#ifndef MEDIA_H // https://www.geeksforgeeks.org/cpp/header-files-in-c-c-with-examples/ (Used the "User-Defined header files")
+#ifndef MEDIA_H // https://www.geeksforgeeks.org/cpp/header-files-in-c-c-with-examples/ (Used the "User-Defined header files") + learncpp.com/cpp-tutorial/header-guards/
                 // if media.h not defined...
                 
 #define MEDIA_H // define. Else continue (Don't want a redefinition).
@@ -23,14 +23,14 @@ using namespace std;
 class Media
 {
     public:
-        Media() {}
-
+        Media();
         virtual ~Media() {}
-
+        
         char* getTitle();
         int getYear();
-
-    private:
+        
+    protected: // Protected allows child classes use the variables. (I originally had it as priavte).
+              // https://www.w3schools.com/cpp/cpp_access_specifiers.asp
         char* title;
         int year;
 };
